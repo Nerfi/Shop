afad8a6e128cad776dd9844cfd40430009cc1975
Juan Paredes <juanchuu.jfpc@gmail.com>
2019-03-30 20:24:36 +0100

    added some style, check what else we can do

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts 'Creating faker'
+
+5.times do
+clothe = Clothe.New(
+  name: Faker::Company.name,
+  price: '10$'
+
+  )
+ clothe.save!
+
+
+  end
+  puts 'Finish'
