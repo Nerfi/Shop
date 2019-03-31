7b2613673d8716f63e5231e89202cd5c4dcafdbd
Juan Paredes <juanchuu.jfpc@gmail.com>
2019-03-31 20:11:58 +0200

    adde sku

---
+++ app/models/teddy.rb
@@ -0,0 +1,3 @@
+class Teddy < ApplicationRecord
+  belongs_to :clothe
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_30_174733) do
+ActiveRecord::Schema.define(version: 2019_03_31_180948) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,4 +22,15 @@ ActiveRecord::Schema.define(version: 2019_03_30_174733) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "teddies", force: :cascade do |t|
+    t.string "sku"
+    t.string "name"
+    t.bigint "clothe_id"
+    t.string "photo_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["clothe_id"], name: "index_teddies_on_clothe_id"
+  end
+
+  add_foreign_key "teddies", "clothes"
 end
