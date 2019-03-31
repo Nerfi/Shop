e57e5dbe9e3ce60a033049ec12ade866fa1b7a85
Juan Paredes <juanchuu.jfpc@gmail.com>
2019-03-31 20:21:51 +0200

    added seeds, working

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,14 @@
-require 'faker'
+puts 'Cleaning database...'
+Teddy.destroy_all
+Clothe.destroy_all
 
-puts 'Creating faker'
+puts 'Creating clothes...'
+geek = Clothe.create!(name: 'geek')
+kids = Clothe.create!(name: 'kids')
 
-5.times do
-clothe = Clothe.New(
-  name: Faker::Company.name,
-  price: '10$'
+puts 'Creating teddies...'
+Teddy.create!(sku: 'original-teddy-bear', name: 'Teddy bear', clothe: kids, photo_url: 'https://images.unsplash.com/photo-1458134580443-fbb0743304eb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=925&q=80')
 
-  )
- clothe.save!
-
-
-  end
-  puts 'Finish'
+Teddy.create!(sku: 'jean-mimi', name: 'Jean-Michel - Le Wagon', clothe: geek, photo_url: 'https://images.unsplash.com/photo-1537160699291-f3e06ab5b6b1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80')
+Teddy.create!(sku: 'octocat',   name: 'Octocat -  GitHub',      clothe: geek, photo_url: 'https://images.unsplash.com/photo-1526743851649-2282229bac05?ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80')
+puts 'Finished'
